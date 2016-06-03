6082aa8930fb3e71891cf2731ff5bf6f8c7b2462
sobhanss <sobiatsain@gmail.com>
2016-06-03 10:59:11 +0430

    Add files via upload

--- plugins/get.lua
+++ plugins/get.lua
@@ -11,18 +11,15 @@ local function get_value(msg, var_name)
     if not value then
       return
     else
-      return var_name..':\n'..value
+      return value
     end
   end
 end
 
 local function run(msg, matches)
-  if not is_momod(msg) then -- only for mods,owner and admins
-    return 
-  end
   if matches[2] then
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /get ".. matches[2])-- save to logs
+    savelog(msg.to.id, name.." ["..msg.from.id.."]".. matches[2])-- save to logs
     return get_value(msg, matches[2])
   else
     return
@@ -31,7 +28,7 @@ end
 
 return {
   patterns = {
-    "^([#!/]get) (.+)$"
+    "^([/#!])(.+)$"
   },
   run = run
 }
