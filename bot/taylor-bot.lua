3651ab3bdb22fd438071a44e6cdc169a8336ad8c
sobhanss <sobiatsain@gmail.com>
2016-06-05 22:54:08 +0430

    Update taylor-bot.lua

--- bot/taylor-bot.lua
+++ bot/taylor-bot.lua
@@ -233,23 +233,11 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {194193264},--Sudo users
+    sudo_users = {148338435},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Taylor Team v4
     Taylor Team and Taylor Bot Anti spam / anti link
     
-    website : 
-    taylortg.ir  ❤️
-    
-    admin : 
-    
-    @negative_officiall ❤️
-    
-    
-    channel : 
-    
-    
-    @taylor Team ❤️
 ]],
     help_text_realm = [[
 Realm Commands:
